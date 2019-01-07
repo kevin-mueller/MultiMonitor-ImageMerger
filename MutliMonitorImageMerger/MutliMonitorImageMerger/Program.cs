655b2cbda180efa83e17641b04ffb69659a950c6
Kevin Müller <kevin.mueller1@gmx.ch>
2019-01-07 15:20:18 +0100

    Clean Up, added comments

--- MutliMonitorImageMerger/MutliMonitorImageMerger/Program.cs
+++ MutliMonitorImageMerger/MutliMonitorImageMerger/Program.cs
@@ -12,13 +12,19 @@ namespace MutliMonitorImageMerger
     {
         static void Main(string[] args)
         {
+            //Here's an example:
+            
+            //Create the List. ( Name of your Monitor, Image )
             Dictionary<string, Image> imagesWithMonitor = new Dictionary<string, Image>();
             imagesWithMonitor.Add(Screen.PrimaryScreen.DeviceName, Image.FromFile(@"C:\Users\Kevin\Pictures\WallpaperChanger\wallhaven-93402.jpg"));
 
+            //Create the Object. The attribute is going to be your final image.
             Merger merger = new Merger(@"C:\Users\Kevin\Desktop\final.png");
+
+            //your final image Path gets returned
             string finalImage = merger.MergeImagesAccordingToMonitors(imagesWithMonitor);
 
-
+            //Additionaly you can set (any) image as Wallpaper using this Method.
             merger.setWallpaperFromFile(finalImage);
         }
     }
